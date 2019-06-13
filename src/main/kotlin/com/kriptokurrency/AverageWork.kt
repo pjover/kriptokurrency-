59952f707f7aaede26cdedb01a491e71acbcdae7
Pere Jover <pjover@hotelbeds.com>
2019-06-13 18:38:34 +0200

    Implement the proof of work system: average work script

---
+++ src/main/kotlin/com/kriptokurrency/AverageWork.kt
@@ -0,0 +1,43 @@
+package com.kriptokurrency
+
+import com.kriptokurrency.bo.Block
+import kotlin.math.roundToInt
+
+
+fun averageWork() {
+
+    val blockchain = getHackedGenesisBlockchain()
+    val times = mutableListOf<Long>()
+
+    for (i in 0 until 1000) {
+        val prevTimestamp = blockchain.chain.last().timestamp
+
+        blockchain.addBlock(listOf("block $i"))
+        val nextBlock = blockchain.chain.last()
+
+        val timeDiff = nextBlock.timestamp - prevTimestamp
+        times.add(timeDiff)
+
+        val average = times.average().roundToInt()
+
+        println("Time to mine block: $timeDiff ms. Difficulty: ${nextBlock.difficulty}. Average time: $average ms")
+    }
+
+}
+
+fun getHackedGenesisBlockchain(): Blockchain {
+    val blockchain = Blockchain()
+
+    blockchain.chain[0] = Block(
+            System.currentTimeMillis(),
+            "-----",
+            "hash-one",
+            listOf("This is the genesis block"),
+            INITIAL_DIFFICULTY,
+            0)
+    return blockchain
+}
+
+fun main() {
+    averageWork()
+}
