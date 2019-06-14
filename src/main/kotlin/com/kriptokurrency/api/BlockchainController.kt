6a791f070058fa86be400b57a1954d6ce585cbd4
Pere Jover <pjover@hotelbeds.com>
2019-06-14 14:48:16 +0200

    post request to mine a block

--- src/main/kotlin/com/kriptokurrency/api/BlockchainController.kt
+++ src/main/kotlin/com/kriptokurrency/api/BlockchainController.kt
@@ -4,9 +4,8 @@ import com.kriptokurrency.core.Block
 import com.kriptokurrency.core.Blockchain
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import io.swagger.annotations.ApiParam
+import org.springframework.web.bind.annotation.*
 
 
 @Api(tags = ["Blockchain"], description = "Blockchain operations")
@@ -16,9 +15,22 @@ class BlockchainController(
         private val blockchain: Blockchain
 ) {
 
-    @ApiOperation(value = "Retrieves the blockchain")
+    @ApiOperation(value = "Returns the blockchain")
     @GetMapping("/blocks")
     fun blocks(): List<Block> {
         return blockchain.chain
     }
+
+    @ApiOperation(value = "Adds a new block to the blockchain", consumes = "application/json")
+    @PostMapping("/mine", consumes = ["application/json"])
+    fun mine(
+            @ApiParam(name = "data", required = true)
+            @RequestBody
+            data: List<String>
+    ): List<Block> {
+
+        blockchain.addBlock(data)
+        return blockchain.chain
+    }
+
 }
--- src/main/kotlin/com/kriptokurrency/core/Constants.kt
+++ src/main/kotlin/com/kriptokurrency/core/Constants.kt
@@ -1,15 +1,13 @@
 package com.kriptokurrency.core
 
-import java.time.Instant
-
 const val MINE_RATE = 1000
 
 const val INITIAL_DIFFICULTY = 3
 
 val GENESIS_BLOCK = Block(
-        Instant.EPOCH.toEpochMilli(),
+        1560511686169,
         "-----",
         "hash-one",
-        listOf("This is the genesis block"),
+        listOf("The genesis block"),
         INITIAL_DIFFICULTY,
         0)
